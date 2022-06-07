3cb99f2929cbde75da773c54dc26801997f0adf9
Yuliia Marchuk <44868283+juliia5m@users.noreply.github.com>
2022-06-07 16:22:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from troll import Troll
+from pirate import Pirate
+
+
+def main():
+    pirate = Pirate()
+    pirate.defend()
+
+    troll = Troll()
+    troll.defend()
+
+
+if __name__ == '__main__':
+    main()
